36c7df8ab9afec6dffce87e37e5078e39720e270
bakr4039-byte <bakr4039@gmail.com>
2025-12-31 22:29:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-const cacheName = 'hader-v3';
-const assets = ['./', './index.html', './manifest.json'];
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open('h1').then(c => c.addAll(['./', './index.html', './manifest.json'])));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
