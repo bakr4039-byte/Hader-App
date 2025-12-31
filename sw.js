aed1ac1497e2b63b98556cf83f60b8c765817c22
bakr4039-byte <bakr4039@gmail.com>
2025-12-31 16:46:48 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'hader-v1';
+const assets = [
+  './',
+  './index.html',
+  './admin.html',
+  './reports.html',
+  './links.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
