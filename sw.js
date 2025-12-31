b5b95c67e27a53ceda9c0ff38dc434065c4b4b00
bakr4039-byte <bakr4039@gmail.com>
2025-12-31 22:41:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('h1').then(c => c.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(
+    caches.open('hader-v4').then(c => c.addAll(['./', './index.html', './manifest.json']))
+  );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
