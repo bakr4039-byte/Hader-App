cc22ec4e412c7791694bbbce95650df055c03b25
bakr4039-byte <bakr4039@gmail.com>
2025-12-31 22:05:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
-const cacheName = 'hader-v2';
-const assets = [
-  './',
-  './index.html',
-  './admin.html',
-  './reports.html',
-  './links.html',
-  './manifest.json',
-  './style.css'
-];
+const cacheName = 'hader-v3';
+const assets = ['./', './index.html', './manifest.json'];
 
-// تثبيت الملفات في الذاكرة
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// استرجاع الملفات بسرعة
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
